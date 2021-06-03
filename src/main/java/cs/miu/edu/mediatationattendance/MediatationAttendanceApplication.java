5fec617f569392097925c937dd9e580d4358718f
Prabhat Gyawali <gyawaliprabhat234@gmail.com>
2021-06-03 16:42:26 -0500

    Init

---
+++ src/main/java/cs/miu/edu/mediatationattendance/MediatationAttendanceApplication.java
@@ -0,0 +1,13 @@
+package cs.miu.edu.mediatationattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediatationAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediatationAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/miu/edu/mediatationattendance/MediatationAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.miu.edu.mediatationattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatationAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
