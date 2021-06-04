b947e1c18da00b751d964d29f7fe40705649b5e2
Prabhat Gyawali <gyawaliprabhat234@gmail.com>
2021-06-03 21:57:59 -0500

    adding some files about jwt token generation

---
+++ src/main/java/cs/miu/edu/mediatationattendance/jwt/JwtResponse.java
@@ -0,0 +1,13 @@
+package cs.miu.edu.mediatationattendance.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+
+    private String jwtToken;
+}
