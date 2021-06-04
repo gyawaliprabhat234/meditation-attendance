b559473dec7baa053fb11692e8973b84061c2bfa
Quoc Trinh <quoc.trinh@outlook.com>
2021-06-04 09:20:43 -0500

    Implement the Authentication

---
+++ src/main/java/cs/miu/edu/mediatationattendance/controller/FacultyController.java
@@ -0,0 +1,16 @@
+package cs.miu.edu.mediatationattendance.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/faculty")
+public class FacultyController {
+    @GetMapping("/")
+    @PreAuthorize("hasAuthority('FACULTY')")
+    public String faculty() {
+        return "Welcome to home page1111!!";
+    }
+}
---
+++ src/main/java/cs/miu/edu/mediatationattendance/jwt/JwtAuthenticationResponse.java
@@ -0,0 +1,17 @@
+package cs.miu.edu.mediatationattendance.jwt;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class JwtAuthenticationResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
