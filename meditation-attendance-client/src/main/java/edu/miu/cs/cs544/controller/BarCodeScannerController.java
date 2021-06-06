c41649d839e4430a7132ef48dbbe56df4c6582bb
Quoc Trinh <quoc.trinh@outlook.com>
2021-06-06 00:34:40 -0500

    Add registry and client app

---
+++ meditation-attendance-client/src/main/java/edu/miu/cs/cs544/controller/BarCodeScannerController.java
@@ -0,0 +1,10 @@
+package edu.miu.cs.cs544.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/barcode")
+class BarCodeScannerController {
+
+}
