d172895a182feca263530ce31b6cb9b34050eb54
Henycake <41394315+Henycake@users.noreply.github.com>
2021-06-07 17:08:05 -0500

    controller

---
+++ meditation-attendance-server/src/main/java/cs/miu/edu/meditationattendance/controller/AdministratorController.java
@@ -0,0 +1,16 @@
+package cs.miu.edu.meditationattendance.controller;
+
+import cs.miu.edu.meditationattendance.dto.AttendanceDTO;
+import cs.miu.edu.meditationattendance.exception.ResourceNotFoundException;
+import cs.miu.edu.meditationattendance.service.AttendanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/attendance")
+public class AdministratorController {
+
+
+}
