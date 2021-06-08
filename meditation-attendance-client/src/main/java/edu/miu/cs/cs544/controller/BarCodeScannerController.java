c0cfbef7c08073e28d6ed983bee7085a45727c79
Quoc Trinh <quoc.trinh@outlook.com>
2021-06-08 11:13:30 -0500

    Clean code

--- meditation-attendance-client/src/main/java/edu/miu/cs/cs544/controller/BarCodeScannerController.java
+++ meditation-attendance-client/src/main/java/edu/miu/cs/cs544/controller/BarCodeScannerController.java
@@ -2,7 +2,7 @@ package edu.miu.cs.cs544.controller;
 
 import edu.miu.cs.cs544.exception.ResourceNotFoundException;
 import edu.miu.cs.cs544.model.AttendanceDTO;
-import edu.miu.cs.cs544.service.AttendanceService;
+import edu.miu.cs.cs544.service.AttendanceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +16,7 @@ import java.util.List;
 class BarCodeScannerController {
 
     @Autowired
-    private AttendanceService attendanceService;
+    private AttendanceServiceImpl attendanceService;
 
     @GetMapping
     public List<AttendanceDTO> getSession(){
--- meditation-attendance-client/src/main/java/edu/miu/cs/cs544/service/AttendanceService.java
+++ meditation-attendance-client/src/main/java/edu/miu/cs/cs544/service/AttendanceService.java
@@ -1,106 +1,16 @@
 package edu.miu.cs.cs544.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.miu.cs.cs544.exception.ResourceNotFoundException;
 import edu.miu.cs.cs544.model.AttendanceDTO;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
-@Log4j2
-public class AttendanceService {
+public interface AttendanceService {
+	List<AttendanceDTO> getSessions();
 
-	public static final String VAR_TMP_DATA_JSON = "/var/tmp/data.json";
-	@Autowired
-	private DiscoveryClient discoveryClient;
+	List<AttendanceDTO> update(Long baCode) throws ResourceNotFoundException;
 
-	@Autowired
-	private RestTemplate restTemplate;
-
-	@Value("${spring-boot-server.name}")
-	private String serverName;
-
-	@Value("${location.bldg}")
-	private String buildingName;
-
-	@Value("${location.room}")
-	private String roomName;
-
-	public List<AttendanceDTO> getSessions() {
-		String url = getBaseServiceUrl() + "/students/session?bldgName=" + buildingName + "&roomName=" +roomName;
-		AttendanceDTO[] forObject = restTemplate.getForObject(url, AttendanceDTO[].class);
-		save(forObject);
-		return readData();
-	}
-
-	public List<AttendanceDTO> update(Long baCode) throws ResourceNotFoundException {
-		updateBarcodeToFile(baCode);
-		return readData();
-	}
-
-	public void importData() {
-		String url = getBaseServiceUrl() + "/attendance/saveAll";
-		List<AttendanceDTO> attendanceDTOS = readData();
-		Boolean result = restTemplate.postForObject(url, attendanceDTOS, Boolean.class);
-		log.info("Save attendance : " + (result ? "sucess" : "falied"));
-	}
-
-	private void updateBarcodeToFile(Long baCode) throws ResourceNotFoundException {
-			List<AttendanceDTO> attendanceDTOS = readData();
-			Optional<AttendanceDTO> att = attendanceDTOS.stream().filter(attendance -> baCode.equals(attendance.getBarCode()))
-					.findAny();
-			if (att.isPresent()){
-				att.ifPresent(a->a.setTimeStamp(LocalDateTime.now()));
-				AttendanceDTO[] forObject = attendanceDTOS.toArray(new AttendanceDTO[attendanceDTOS.size()]);
-				save(forObject);
-			} else{
-				throw new ResourceNotFoundException("Student is not in this class");
-			}
-	}
-
-	private List<AttendanceDTO> readData() {
-		File file = new File(VAR_TMP_DATA_JSON);
-		ObjectMapper objectMapper = new ObjectMapper();
-		List<AttendanceDTO> attendanceDTOS = new ArrayList<>();
-		try {
-			attendanceDTOS = objectMapper.readValue(file, new TypeReference<List<AttendanceDTO>>() {});
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return attendanceDTOS;
-	}
-
-	private void save(AttendanceDTO[] forObject) {
-		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			File convertFile = new File(VAR_TMP_DATA_JSON);
-			if (convertFile.exists()) {
-				convertFile.delete();
-			}
-			convertFile.createNewFile();
-			FileOutputStream fout = new FileOutputStream(convertFile);
-			objectMapper.writeValue(fout, forObject);
-		} catch (IOException e) {
-			log.error("Cannot write data to file", e);
-		}
-	}
-
-	private String getBaseServiceUrl() {
-		List<ServiceInstance> serviceInstances = discoveryClient.getInstances(serverName);
-		return serviceInstances.get(0).getUri().toString();
-	}
+	void importData();
 }
